ecf6862b4785479e1d646ed3ab4a1e0fdbe85f63
Isaac Wilson <isaac.wilson1811@stu.mtec.edu>
2025-03-21 12:49:40 -0600

    Add selector and method to verify dog images bug

--- test/pageobjects/inventory.js
+++ test/pageobjects/inventory.js
@@ -12,6 +12,12 @@ class Inventory extends Base {
     get linkLogout () {
         return $('//a[@data-test="logout-sidebar-link"]')
     }
+    get imagesProblem () {
+        // $$ selector returns an array of matches.
+        // The bug that occurs (and is expected) with problem_user is
+        // the same incorrect image of a dog being used for every single product.
+        return $$('//div[@class="inventory_item_img"]//img[@src="/static/media/sl-404.168b1cce.jpg"]')
+    }
 
     navigateToPage () {
         return super.navigateTo('inventory')
@@ -24,6 +30,13 @@ class Inventory extends Base {
     async logOut () {
         await this.buttonMenu.click()
         await this.linkLogout.click()
+    }
+
+    async verifyProblemImages () {
+        let array = await this.imagesProblem
+        // There are always 6 duplicate images. But one or more is expected.
+        // await(array.length).toBe(6)
+        await expect(array.length).toBeGreaterThanOrEqual(1)
     }
 
 }
