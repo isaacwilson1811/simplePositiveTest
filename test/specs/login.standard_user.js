d28e64114c0e7f8469658192ab03f86cd38c0829
Isaac Wilson <isaac.wilson1811@stu.mtec.edu>
2025-03-21 11:21:33 -0600

    rename test for standard user, add negative login case

--- test/specs/login.standard_user.js
+++ test/specs/login.standard_user.js
@@ -1,13 +1,21 @@
 import HomePage from '../pageobjects/homepage.js'
 import Inventory from '../pageobjects/inventory.js'
 
+// Positive Login
 describe('Log in as standard user', () => {
     it('Should log in succesfully', async () => {
         await HomePage.navigateToPage()
         await HomePage.login('standard_user','secret_sauce')
-
-        
         await Inventory.verifyLoggedIn()
+    })
+})
+
+// Negative Login
+describe('Log in as standard user', () => {
+    it('Should log in succesfully', async () => {
+        await HomePage.navigateToPage()
+        await HomePage.login('standard_user','incorrect_password')
         await Inventory.navigateToPage()
+        // Add and use a method in page object inventory to expect navigating to inventory to fail
     })
 })
