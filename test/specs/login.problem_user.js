bcb5afb0a28f6dc3f18b836bde487caf02263970
Isaac Wilson <isaac.wilson1811@stu.mtec.edu>
2025-03-21 12:52:53 -0600

    Add problem user test

---
+++ test/specs/login.problem_user.js
@@ -0,0 +1,33 @@
+import HomePage from '../pageobjects/homepage.js'
+import Inventory from '../pageobjects/inventory.js'
+
+const userName = 'problem_user'
+const password = 'secret_sauce'
+const wrongPassword = 'incorrect_password'
+
+// Positive Login
+describe('Log in as problem_user', () => {
+    it('Should log in succesfully, and see the dog image for every product', async () => {
+        await HomePage.navigateToPage()
+        await HomePage.login(userName, password)
+        await Inventory.verifyLoggedIn()
+        await Inventory.verifyProblemImages()
+    })
+})
+
+// Log out
+describe('Log out from problem_user', () => {
+    it('Should log out succesfully', async () => {
+        await Inventory.logOut()
+        await HomePage.verifyNotLoggedIn()
+    })
+})
+
+// Negative Login
+describe('Log in as problem_user with incorrect_password', () => {
+    it('Should get No Match Error', async () => {
+        await HomePage.navigateToPage()
+        await HomePage.login(userName, wrongPassword)
+        await HomePage.verifyErrorNoMatch()
+    })
+})
