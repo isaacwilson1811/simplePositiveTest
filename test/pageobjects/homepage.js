17610e219260c8dff138c151b62bead01ab80f14
Isaac Wilson <isaac.wilson1811@stu.mtec.edu>
2025-03-21 11:56:06 -0600

    add selectors and methods to verify not logged in and login error

--- test/pageobjects/homepage.js
+++ test/pageobjects/homepage.js
@@ -2,6 +2,7 @@ import { $ } from '@wdio/globals'
 import Base from './base.js'
 
 class HomePage extends Base {
+
     get inputUsername () {
         return $('//input[@data-test="username"]')
     }
@@ -11,9 +12,20 @@ class HomePage extends Base {
     get inputLoginButton () {
         return $('//input[@data-test="login-button"]')
     }
+    get elementErrorLogin () {
+        return $('//*[@data-test="error"][contains(text(),"Username and password do not match")]')
+    }
 
     navigateToPage () {
         return super.navigateTo('')
+    }
+
+    async verifyNotLoggedIn () {
+        await expect(this.inputLoginButton).toBeExisting()
+    }
+
+    async verifyErrorLogin () {
+        await expect(this.elementErrorLogin).toBeExisting()
     }
 
     async login (username, password) {
